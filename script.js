77c5d1c04693ed9d9061865c283cdaa23fd98d2e
Stella Del Mar <113214552+Stelladelmar@users.noreply.github.com>
2023-05-30 17:06:19 +0100

    episode season and episode failed try
    episode season and episode failed try

--- script.js
+++ script.js
@@ -42,21 +42,27 @@ episodeList.forEach(episode => {
   episodeNameHolder.appendChild(episodeName);
 
    // putting the title inside episode name by accessing the data
-  
+   episodeName.innerHTML = episode.name;
    //You should combine season number and episode number into an episode code:
   //Each part should be zero-padded to two digits.
   //Example: S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
 
-  episodeName.innerHTML = episode.name;
+  const episodeNameNumHolder = document.createElement("div");
+    episodeNameNumHolder.classList.add("episode-name-num-holder");
+    episodeCard.appendChild(episodeNameNumHolder);
 // //**** *//DO-NOT-DELETE-BELOW-UNTIL-CHECKING-IT-WORKS-********
 //   const paddedSeasonNum = ("0" + episode.season).slice(-2);
 //   const paddedEpisodeNum = ("0" + episode.number).slice(-2);
 //   const episodeName = episode.name + paddedSeasonNum + paddedEpisodeNum;
 // //**** */DO-NOT-DELETE-ABOVE-UNTIL-CHECKING-IT-WORKS-********
-  const paddedSeasonNum = ("0" + episode.season).slice(-2);
-  const paddedEpisodeNum = ("0" + episode.number).slice(-2);
-  const theEpisodeName = episode.name + paddedSeasonNum + paddedEpisodeNum;
-
+  //***************
+const episodeNameNumber = document.createElement("h2");
+episodeNameNumber.classList.add("episode-name-number");
+episodeNameNumHolder.appendChild(episodeNameNumber);
+const paddedSeasonNum = ("0" + episodeList[i].season).slice(-2);
+const paddedEpisodeNum = ("0" + episodeList[i].number).slice(-2);
+const detailedEpisodeName =episode.name + paddedSeasonNum +paddedEpisodeNum;
+// episodeNameNumHolder.innerText = `${episodeList[i].name}- S${paddedSeasonNum}E${paddedEpisodeNum}`;
 
   // create episode image holder inside episode card
   const episodeImageHolder = document.createElement("div");
