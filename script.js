3811909596e86feeb98d5339f973ab6acadb532c
Stella Del Mar <113214552+Stelladelmar@users.noreply.github.com>
2023-06-01 23:54:34 +0100

    lvl100 function
    lvl100 function

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 //You can edit ALL of the code here
 const rootHtml = document.getElementById("root");
-const episodeCard = document.createElement("div");
-// episodeCard.classList.add("episode-card");
-// rootHtml.appendChild(episodeCard);
-
-// const episodeNameHolder = document.createElement("div");
-// episodeNameHolder.classList.add("episode-name-holder");
-// episodeCard.appendChild(episodeNameHolder);
-
-// const episodeName =document.createElement("span");
-// episodeName.classList.add("episode-name-text");
-// episodeNameHolder.appendChild(episodeName);
-// episodeName.innerHTML = episode.name;
+///////// const episodeCard = document.createElement("div");
 
 function setup() {
   const allEpisodes = getAllEpisodes();
@@ -22,54 +11,47 @@ function makePageForEpisodes(episodeList) {
 
   //getting Root Element from index.html
   const rootHtml = document.getElementById("root");
-  rootHtml.textContent = `Got ${episodeList.length} episode(s)`;
-  
-episodeList.forEach(episode => {
+  //////////// rootHtml.textContent = `Got ${episodeList.length} episode(s)`;
+
+  const episodeCardsContainer = document.createElement("div")////
+  episodeCardsContainer.classList.add("episode-cards-container");
+   rootHtml.appendChild(episodeCardsContainer); ////
+
+   episodeList.forEach(episode => {
 
   // creating episode card inside root element
+  const episodeCard = document.createElement("div");//////
   episodeCard.classList.add("episode-card");
-  rootHtml.appendChild(episodeCard);
+  episodeCardsContainer.appendChild(episodeCard);
+  // rootHtml.appendChild(episodeCard);///////
    
   // creating episode name holder inside episode card
-  const episodeNameHolder = document.createElement("div");
-  episodeNameHolder.classList.add("episode-name-holder");
-  episodeCard.appendChild(episodeNameHolder);
+  const episodeTitleHolder = document.createElement("div");
+  episodeTitleHolder.classList.add("episode-name-holder");
+  episodeCard.appendChild(episodeTitleHolder);
   
   // creating episode name inside episode name holder
 
-  const episodeName = document.createElement("span");
-  episodeName.classList.add("episode-name-text");
-  episodeNameHolder.appendChild(episodeName);
+  const episodeTitleTextHtml = document.createElement("span");////
+  episodeTitleTextHtml.classList.add("episode-name-text");////
+  episodeTitleHolder.appendChild(episodeTitleTextHtml);////
 
-   // putting the title inside episode name by accessing the data
-  //  episodeName.innerHTML = episode.name;
+   // putting the title inside episode name by accessing the data;
    //You should combine season number and episode number into an episode code:
   //Each part should be zero-padded to two digits.
   //Example: S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
 
-  const episodeNameNumHolder = document.createElement("div");
-    episodeNameNumHolder.classList.add("episode-name-num-holder");
-    episodeCard.appendChild(episodeNameNumHolder);
-// //**** *//DO-NOT-DELETE-BELOW-UNTIL-CHECKING-IT-WORKS-********
-//   const paddedSeasonNum = ("0" + episode.season).slice(-2);
-//   const paddedEpisodeNum = ("0" + episode.number).slice(-2);
-//   const episodeName = episode.name + paddedSeasonNum + paddedEpisodeNum;
-// //**** */DO-NOT-DELETE-ABOVE-UNTIL-CHECKING-IT-WORKS-********
-  //***************
-const episodeNameNumber = document.createElement("h2");
-episodeNameNumber.classList.add("episode-name-number");
-episodeNameNumHolder.appendChild(episodeNameNumber);
-// const paddedSeasonNum = ("0" + episodeList[i].season).slice(-2);
-// const paddedEpisodeNum = ("0" + episodeList[i].number).slice(-2);
-// const detailedEpisodeName =episode.name + paddedSeasonNum +paddedEpisodeNum;
-// episodeName.innerText = `${episodeList[i].name}- S${paddedSeasonNum}E${paddedEpisodeNum}`;
+  const episodeName = episode.name;
+    const paddedSeasonNum = ("0" + episode.season).slice(-2);
+    const paddedEpisodeNum = ("0" + episode.number).slice(-2);
+    episodeTitleTextHtml.textContent = `${episodeName} - S${paddedSeasonNum}E${paddedEpisodeNum}`;
 
   // create episode image holder inside episode card
   const episodeImageHolder = document.createElement("div");
   episodeImageHolder.classList.add("episode-image-holder");
   episodeCard.appendChild(episodeImageHolder);
 
-  //creating episode image inside image holder
+   //creating episode image inside image holder
   const episodeImage = document.createElement("img");
   episodeImage.classList.add("episode-image-text");
   episodeImageHolder.appendChild(episodeImage);
@@ -77,13 +59,12 @@ episodeNameNumHolder.appendChild(episodeNameNumber);
   // putting the title inside episode image by accessing the data
   episodeImage.src = episode.image.medium;
 
-  //
-  // create episode Summary holder inside episode card
+   // create episode Summary holder inside episode card
   const episodeSummaryHolder = document.createElement("div");
   episodeSummaryHolder.classList.add("episode-Summary-holder");
   episodeCard.appendChild(episodeSummaryHolder);
 
-  //creating episode Summary inside Summary holder
+    //creating episode Summary inside Summary holder
   const episodeSummary = document.createElement("p");
   episodeSummary.classList.add("episode-Summary-text");
   episodeSummaryHolder.appendChild(episodeSummary);
